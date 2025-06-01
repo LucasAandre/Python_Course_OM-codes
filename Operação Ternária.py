500bb95dff7a434f99ca391f2ce4016d66a915c9
LucasAandre <92192954+LucasAandre@users.noreply.github.com>
2025-06-01 15:25:08 -0300

    Create Operação Ternária.py

---
+++ Operação Ternária.py
@@ -0,0 +1,15 @@
+'''
+Operação ternária (condicional de uma linha)
+<valor> if <condição> else <outro valor>
+'''
+
+a = 3
+b = 5
+soma = a + b
+
+print('>= 8' if soma >= 8 else '<8') # >= 8
+
+# ou
+
+variavel = '>= 8' if soma >= 8 else '<8'
+print(variavel) # >= 8
