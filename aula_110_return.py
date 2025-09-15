d22ca9995a630769cc0f925bf6918770180485a8
Lucas André <lucas.aandre11@outlook.com>
2025-09-15 18:57:26 -0300

    adicionando exercicios de funcoes

--- aula_110_return.py
+++ aula_110_return.py
@@ -11,3 +11,51 @@ def soma(x, y):
     return x + y
 
 print(1, 2, 3, 4, 5) # 1 2 3 4 5
+
+soma(x, y) # Não aparece nada na tela
+
+print(soma(x, y)) # 3
+
+def sub(*args):
+    print(args)
+
+sub(1, 2, 3, 4, 5, 6, 7) # (1, 2, 3, 4, 5, 6, 7) -> uma tupla
+
+# Brincando um pouco:
+
+def lucas(*args):
+    total = 0
+    for x in args:
+        print(x)
+        total += x
+    return total
+
+resultado = lucas(10, 20, 30)
+print('Total =', resultado)
+
+"""
+10
+20
+30
+Total = 60
+"""
+
+# ou
+
+print(sum((10, 20, 30))) # A função sum() funciona apenas com iterável (lista, tupla, etc)
+
+
+# Com desempacotamento de valores
+def mult(*args): # *args empacota em uma tupla
+    total = 1
+    for numero in args:
+        print(numero, end=' ') # 1 2 3 4 5 6 
+        total *= numero
+    
+    print('\n')
+    return total
+
+numeros = (1, 2, 3, 4, 5, 6)
+result = mult(*numeros) # Desempacotamento da tupla numeros
+
+print(result) # 720
---
+++ aula_113_exerc_return.py
@@ -0,0 +1,9 @@
+'''
+EXERCÍCIOS COM FUNÇÕES
+
+1. Crie uma função que multiplica todos os argumentos não nomeados recebidos. Retorne o 
+total para uma variável e mostre o valor da variável.
+
+2. Crie uma função que diga se um número é par ou ímpar. Retorne se o número é par ou 
+ímpar.
+'''
