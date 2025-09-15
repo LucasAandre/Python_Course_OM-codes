f3734eaddf428115d28dae1e4e30f3ebce60f4c0
Lucas André <lucas.aandre11@outlook.com>
2025-09-15 17:54:30 -0300

    segundo commit

---
+++ aula_110_return.py
@@ -0,0 +1,13 @@
+'''
+args - Argumentos não nomeados
+* - *args (empacotamento e desempacotamento)
+'''
+
+# Lembre-te de desempacotamento
+x, y, *resto = 1, 2, 3, 4
+print(x, y, resto) # 1 2 [3, 4]
+
+def soma(x, y):
+    return x + y
+
+print(1, 2, 3, 4, 5) # 1 2 3 4 5
