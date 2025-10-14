7aecb4fb3d2c8a053d2a20537b8ff0fc863d4b4c
Lucas André <lucas.aandre11@outlook.com>
2025-10-14 19:06:32 -0300

    exercicio numeros que se repetem - set

--- exercicio_set.py
+++ exercicio_set.py
@@ -12,3 +12,25 @@ Exemplo:
     [1, 2, 3, 3, 2, 1] -> 1, 2 e 3 são duplicado (retorne 3)
     [1, 2, 3, 4, 5, 6] -> não tem itens duplicados (retorne -1)
 """
+
+# Não entendi muito bem, mas vamos lá...
+
+s1 = set()
+s2 = set()
+
+def validacao():
+    s3 = s1 & s2
+    return s3
+
+for i in range(1, 4):
+    n = int(input(f'Digite o {i}º número inteiro: '))
+    s1.add(n)
+
+for i in range(4, 7):
+    n = int(input(f'Digite o {i}º número inteiro: '))
+    s2.add(n)
+
+print('\nConjunto 1: ',s1)
+print('\nConjunto 2: ',s2)
+
+print(f'\nHá {len(validacao())} número(s) que se repete(m): {validacao()}')
